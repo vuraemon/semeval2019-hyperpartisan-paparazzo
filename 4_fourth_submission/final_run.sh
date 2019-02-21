aa31350efdc9029b2b983873fc23d6840bb029e5
ngannlt <46961613+ngannlt@users.noreply.github.com>
2019-02-22 04:54:02 +0700

    Update final_run.sh

--- 4_fourth_submission/final_run.sh
+++ 4_fourth_submission/final_run.sh
@@ -7,11 +7,4 @@ o) OUTPUT=${OPTARG};;
 esac
 done
 export PATH=anaconda3/bin:$PATH
-python final_main_1.py -i $INPUT -o $OUTPUT
-python final_main_2.py -i $INPUT -o $OUTPUT
-python final_main_3.py -i $INPUT -o $OUTPUT
-python final_main_4.py -i $INPUT -o $OUTPUT
-python final_main_5.py -i $INPUT -o $OUTPUT
-python final_main_6.py -i $INPUT -o $OUTPUT
-python final_main_7.py -i $INPUT -o $OUTPUT
-python final_main_8.py -i $INPUT -o $OUTPUT
+python final_main.py -i $INPUT -o $OUTPUT
